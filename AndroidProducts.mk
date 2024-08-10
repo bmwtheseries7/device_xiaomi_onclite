add3c698166d27b0e3191f7413257c7670873a1e
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2024-08-10 11:58:57 +0700

    onclite: Afterlife Never Die
    Change-Id: Icb7458b9ec20b97149fe2ec3882031fb786fd9f8

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,2 +1,7 @@
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_onclite.mk
+    $(LOCAL_DIR)/afterlife_onclite.mk
+
+COMMON_LUNCH_CHOICES := \
+    afterlife_onclite-user \
+    afterlife_onclite-userdebug \
+    afterlife_onclite-eng
--- afterlife_onclite.mk
+++ afterlife_onclite.mk
@@ -12,11 +12,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_p.mk)
 # Inherit from onclite device
 $(call inherit-product, device/xiaomi/onclite/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterlifeOS stuff.
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
+
+# Device configs
+AFTERLIFE_MAINTAINER := bmwtheseries
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_BOOT_ANIMATION_RES = 1080
+
+# Include our private certificate
+-include vendor/atiga-priv/keys/keys.mk
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_onclite
+PRODUCT_NAME := afterlife_onclite
 PRODUCT_DEVICE := onclite
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 7
