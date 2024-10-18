VSArchive/android_device_xiaomi_onclite

onclite: Specify AVB recovery flags
Signed-off-by: Jyotiraditya <imjyotiraditya@pm.me>

BoardConfig.mk
@@ -40,6 +40,10 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 # AVB
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000

bmwtheseries7/device_xiaomi_onclite

Change-Id: I576883b03b9c4239f9010bbc200179958e6fe95c

BoardConfig.mk
@@ -41,6 +41,10 @@ BUILD_BROKEN_USES_BUILD_COPY_HEADERS := true
 # AVB
 BOARD_AVB_ENABLE := true
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --set_hashtree_disabled_flag
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa4096.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA4096
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 
 # Kernel
 BOARD_KERNEL_BASE := 0x80000000