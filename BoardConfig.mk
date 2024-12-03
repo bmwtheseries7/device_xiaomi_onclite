LineageOS/android_device_xiaomi_mido

mido: update rootdir from caf
Change-Id: I278e58a1ee728f21187c1b4744ac37cd365cf92d

BoardConfig.mk
@@ -199,7 +199,7 @@ BOARD_RIL_CLASS := ../../../$(LOCAL_PATH)/ril
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(LOCAL_PATH)/rootdir/recovery/fstab.qcom
 
 # Sensor
 USE_SENSOR_MULTI_HAL := true

bmwtheseries7/device_xiaomi_onclite

BoardConfig.mk
@@ -203,7 +203,7 @@ BOARD_RIL_CLASS := ../../../$(DEVICE_PATH)/ril
 TARGET_RIL_VARIANT := caf
 
 # Recovery
-TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/fstab.qcom
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/recovery/fstab.qcom
 
 # Sensor
 USE_SENSOR_MULTI_HAL := true