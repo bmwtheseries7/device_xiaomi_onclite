6abd698cdbe86e4e0bb0d91216c7a71d0807574f
BayerischeMotorenWerke <syaifulamri199@gmail.com>
2024-12-07 07:01:08 +0700

    onclite: Gapps Build
    Change-Id: I537dcc34d1407006bed53a2449cf2173c882ca40

--- afterlife_onclite.mk
+++ afterlife_onclite.mk
@@ -20,6 +20,7 @@ AFTERLIFE_MAINTAINER := bmwtheseries
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_BOOT_ANIMATION_RES = 1080
 TARGET_USE_ECHO := true
+AFTERLIFE_GAPPS := true
 
 # Include our private certificate
 -include vendor/atiga-priv/keys/keys.mk
